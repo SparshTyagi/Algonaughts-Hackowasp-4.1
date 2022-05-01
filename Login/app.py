c10cb8b1de42c51dab20f2289cdbdf26a62f5f77
SparshTyagi <sparshtyagi26@gmail.com>
2022-05-01 15:00:26 +0530

    More adjustments

--- Login/app.py
+++ Login/app.py
@@ -19,9 +19,9 @@ class User(db.Model):
 @app.route('/', methods=['GET'])
 def index():
     if session.get('logged_in'):
-        return render_template('Webpages\home.html')
+        return render_template('home.html')
     else:
-        return render_template('Webpages\index.html', message="Hello!")
+        return render_template('index.html', message="Hello!")
 
 
 @app.route('/register/', methods=['GET', 'POST'])
@@ -32,15 +32,15 @@ def register():
             db.session.commit()
             return redirect(url_for('login'))
         except:
-            return render_template('Webpages\index.html', message="User Already Exists")
+            return render_template('index.html', message="User Already Exists")
     else:
-        return render_template('Webpages\register.html')
+        return render_template('register.html')
 
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template('Webpages\login.html')
+        return render_template('login.html')
     else:
         u = request.form['username']
         p = request.form['password']
@@ -48,7 +48,7 @@ def login():
         if data is not None:
             session['logged_in'] = True
             return redirect(url_for('index'))
-        return render_template('Webpages\index.html', message="Incorrect Details")
+        return render_template('index.html', message="Incorrect Details")
 
 
 @app.route('/logout', methods=['GET', 'POST'])
