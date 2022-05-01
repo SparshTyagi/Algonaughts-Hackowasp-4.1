d8ab9df2882e987c751955b19c89e62c2d5657df
SparshTyagi <sparshtyagi26@gmail.com>
2022-05-01 12:12:32 +0530

    Backend corrections

--- Login/app.py
+++ Login/app.py
@@ -8,47 +8,47 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True)
+    rollno = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
 
-    def __init__(self, username, password):
-        self.username = username
+    def __init__(self, rollno, password):
+        self.rollno = rollno
         self.password = password
 
 
 @app.route('/', methods=['GET'])
 def index():
     if session.get('logged_in'):
-        return render_template('home.html')
+        return render_template('Webpages\home.html')
     else:
-        return render_template('index.html', message="Hello!")
+        return render_template('Webpages\index.html', message="Hello!")
 
 
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         try:
-            db.session.add(User(username=request.form['username'], password=request.form['password']))
+            db.session.add(User(rollno=request.form['rollno'], password=request.form['password']))
             db.session.commit()
             return redirect(url_for('login'))
         except:
-            return render_template('index.html', message="User Already Exists")
+            return render_template('Webpages\index.html', message="User Already Exists")
     else:
-        return render_template('register.html')
+        return render_template('Webpages\register.html')
 
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template('login.html')
+        return render_template('Webpages\login.html')
     else:
-        u = request.form['username']
+        u = request.form['rollno']
         p = request.form['password']
-        data = User.query.filter_by(username=u, password=p).first()
+        data = User.query.filter_by(rollno=u, password=p).first()
         if data is not None:
             session['logged_in'] = True
             return redirect(url_for('index'))
-        return render_template('index.html', message="Incorrect Details")
+        return render_template('Webpages\index.html', message="Incorrect Details")
 
 
 @app.route('/logout', methods=['GET', 'POST'])
